91942ce6fd3afbeb675a9f8c98ff1dcc84fc8965
second1996 <vital10012@gmail.com>
2021-05-10 13:17:46 +0300

    Home page

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ function browsersync() {
 		ghostMode: { clicks: false },
 		notify: false,
 		online: true,
-		// tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
+		tunnel: 'rozkvit', // Attempt to use the URL https://rozkvit.loca.lt
 	})
 }
 
