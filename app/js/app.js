7161bd389d5f07083246092dc88f214679bd7c39
second1996 <vital10012@gmail.com>
2021-05-19 10:56:40 +0300

    Added AOS library. Refactoring code

--- app/js/app.js
+++ app/js/app.js
@@ -3,8 +3,9 @@ import $ from 'jquery'
 window.jQuery = $
 window.$ = $
 
-// // Import vendor jQuery plugin example (not module)
-// require('~/app/libs/mmenu/dist/mmenu.js')
+// Import vendor jQuery plugin example (not module)
+require('../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js') // Popper lib
+require('../../node_modules/jquery-mask-plugin/dist/jquery.mask.min.js') // jQuery Mask input
 
 // Import Swiper slider bundle
 import SwiperCore, { Navigation, Pagination, EffectFade, Autoplay, Swiper } from 'swiper/core'
@@ -13,11 +14,62 @@ SwiperCore.use([Navigation, Pagination, EffectFade, Autoplay])
 // Import Popper.js
 import { createPopper } from '@popperjs/core'
 
-// Import vendor jQuery plugin example (not module)
-require('../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js') // Popper lib
-require('../../node_modules/jquery-mask-plugin/dist/jquery.mask.min.js') // jQuery Mask input
+// Import AOS library
+import AOS from 'aos'
 
 document.addEventListener('DOMContentLoaded', () => {
+	/**
+	 * Preloader
+	 */
+	function loadbar() {
+		let overlay  = document.querySelector('#preloader'),
+				progress = document.querySelector('#preloader-progress'),
+				status   = document.querySelector('#preloader-status'),
+				img      = document.images,
+				counter  = 0,
+				total    = img.length;
+
+		function imgLoaded() {
+			counter += 1
+			const percent = ((100/total*counter) << 0) + '%'
+
+			progress.style.width = percent
+			status.innerHTML = percent
+
+			if (counter === total) return doneLoading()
+		}
+		function doneLoading() {
+			setTimeout(() => {
+				overlay.style.opacity = 0
+			}, 600)
+			setTimeout(() => {
+				overlay.remove()
+			}, 1200)
+		}
+		for(let i = 0; i < total; i++) {
+			let tImg = new Image()
+
+			tImg.onload  = imgLoaded
+			tImg.onerror = imgLoaded
+			tImg.src     = img[i].src
+		}
+	}
+	loadbar()
+
+
+	/**
+	 * Animate on scroll
+	 */
+	AOS.init({
+		once: true,
+		duration: 1000,
+		disable: function () {
+			var maxWidth = 1240;
+			return window.innerWidth < maxWidth;
+		}
+	})
+
+
 	/**
 	 * jQuery input mask
 	 */
@@ -27,7 +79,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	/**
 	 * Smooth scroll
 	 */
-	$('a[data-anchor]').bind('click.smoothscroll', function(){
+	$('a[data-anchor]').bind('click.smoothscroll', function() {
 		const headerHeight = $('.header').outerHeight() - 1
 		const target = $(this).attr('href')
 		const bl_top = $(target).offset().top - headerHeight
